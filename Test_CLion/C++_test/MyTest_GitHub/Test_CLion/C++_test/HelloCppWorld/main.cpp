fa0989eeb1d47d3bb664d329437623cfeba86861
AndreaVoltan <andre.voltan19@gmail.com>
2020-04-22 16:47:13 +0200

    removal of MyTest copy within MyTest

--- Test_CLion/C++_test/MyTest_GitHub/Test_CLion/C++_test/HelloCppWorld/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello World! This is C++" << std::endl;
-    std::cout << "After my first commit in clion, I change this line and switch to the other branch." << std::endl;
-    return 0;
-}
