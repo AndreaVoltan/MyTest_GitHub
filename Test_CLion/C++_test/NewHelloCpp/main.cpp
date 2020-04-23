bdfb243bc7b72d78d681befff556bd0205ca9f8a
AndreaVoltan <andre.voltan19@gmail.com>
2020-04-23 10:39:29 +0200

    Some modifications to Master Branch

--- Test_CLion/C++_test/NewHelloCpp/main.cpp
+++ Test_CLion/C++_test/NewHelloCpp/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Attempt to commit from CLion" << std::endl;
+    std::cout << "Hello, World! This comes from the Master Branch!" << std::endl;
+    std::cout << "How can I automatically reload the CMake profile after switched to another branch? " << std::endl;
     return 0;
 }
