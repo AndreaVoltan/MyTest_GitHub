abc38fb9b5bd71a2793221ead783b40532dd566b
AndreaVoltan <andre.voltan19@gmail.com>
2020-04-23 16:32:43 +0200

    update of main

--- Test_CLion/C++_test/Cpp2share/main.cpp
+++ Test_CLion/C++_test/Cpp2share/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Cpp to share. This line has been written on the Testing Branch and It's still only on the testing branch!" << std::endl;
+    std::cout << "Cpp to share. From testing to master!" << std::endl;
     return 0;
 }
