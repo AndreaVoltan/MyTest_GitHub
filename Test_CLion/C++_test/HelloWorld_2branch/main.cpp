44e419b241fdc440b280a8c8cdabefbc85cce870
AndreaVoltan <andre.voltan19@gmail.com>
2020-04-23 15:28:03 +0200

    some changes to the Cpp2share now on master branch

--- Test_CLion/C++_test/HelloWorld_2branch/main.cpp
+++ Test_CLion/C++_test/HelloWorld_2branch/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World! It's the testing branch that host this code!" << std::endl;
+    std::cout << "Hello, World! The testing branch initially hosted this code. Now it's shared in both branches!" << std::endl;
     return 0;
 }
