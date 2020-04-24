fa4586fe0b13b2c0cce690e1f90731d40cec1a6f
AndreaVoltan <andre.voltan19@gmail.com>
2020-04-24 09:29:39 +0200

    main.cpp ready for 1st merge

--- Test_CLion/C++_test/Cpp2share/main.cpp
+++ Test_CLion/C++_test/Cpp2share/main.cpp
@@ -2,6 +2,6 @@
 
 int main() {
     std::cout << "Cpp to share. From testing to master!" << std::endl;
-    std::cout << "If the merge is successful write OK:  ____" << std::endl;
+    std::cout << "If the merge is successful write OK:  ___" << std::endl;
     return 0;
 }
